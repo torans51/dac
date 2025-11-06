55fa390f90a35c5a05e56492ade5beba0aa729a3
torans <>
2025-11-06 10:06:31 +0100

    Huge refactoring: replace 'dac' with 'dac *' in all function signatures

--- dac.h
+++ dac.h
@@ -27,40 +27,39 @@ typedef struct {
 } dac;
 
 /* Public *********************************************************************/
-dac dac_new(char *s);
+dac *dac_new(char *s);
 void dac_init(dac *s, char *str);
-size_t dac_len(dac s);
-char *dac_to_cstr(dac s);
-bool dac_eq(dac s1, dac s2);
-void dac_append(dac *dest, dac str);
-bool dac_contains(dac *s, dac search);
-int dac_find(dac *s, dac search);
-void dac_replace(dac *s, dac search, dac str);
-void dac_replace_all(dac *s, dac search, dac str);
-void dac_append_many(dac *dest, dac items[], size_t items_count);
-dac dac_join(dac items[], size_t ietms_count, dac delim);
-bool dac_starts_with(dac s, dac prefix);
-bool dac_ends_with(dac s, dac suffix);
+size_t dac_len(dac *s);
+char *dac_to_cstr(dac *s);
+bool dac_eq(dac *s1, dac *s2);
+void dac_append(dac *dest, dac *str);
+bool dac_contains(dac *s, dac *search);
+int dac_find(dac *s, dac *search);
+void dac_replace(dac *s, dac *search, dac *str);
+void dac_replace_all(dac *s, dac *search, dac *str);
+void dac_append_many(dac *dest, dac *items[], size_t items_count);
+dac *dac_join(dac **items, size_t ietms_count, dac *delim);
+bool dac_starts_with(dac *s, dac *prefix);
+bool dac_ends_with(dac *s, dac *suffix);
 void dac_free(dac *s);
-void dac_free_many(dac items[], size_t count);
+void dac_free_many(dac **items, size_t count);
 /******************************************************************************/
 
 /* Private ********************************************************************/
 void dac_reserve_capacity(dac *s, size_t expected_capacity);
 /******************************************************************************/
 
-// #define DAC_IMPLEMENTATION 1
 #ifdef DAC_IMPLEMENTATION
 
 #define DAC_DEFAULT_CAP 256
 #define DAC_ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
 
-dac dac_new(char *s) {
-  dac d = {0};
+dac *dac_new(char *s) {
+  dac *d = malloc(sizeof(dac));
   size_t len = strlen(s) + 1;
-  dac_reserve_capacity(&d, len);
-  memcpy(d.ptr, s, len);
-  d.count = len;
+  dac_reserve_capacity(d, len);
+  memcpy(d->ptr, s, len);
+  d->count = len;
   return d;
 }
 
@@ -71,38 +70,38 @@ void dac_init(dac *d, char *s) {
   d->count = count;
 }
 
-size_t dac_len(dac s) { return s.count - 1; }
+size_t dac_len(dac *s) { return s->count > 0 ? s->count - 1 : 0; }
 
-char *dac_to_cstr(dac s) { return s.ptr; }
+char *dac_to_cstr(dac *s) { return s->ptr; }
 
-bool dac_eq(dac s1, dac s2) {
+bool dac_eq(dac *s1, dac *s2) {
   if (dac_len(s1) != dac_len(s2))
     return false;
 
   for (size_t i = 0; i < dac_len(s1); i++) {
-    if (s1.ptr[i] != s2.ptr[i])
+    if (s1->ptr[i] != s2->ptr[i])
       return false;
   }
   return true;
 }
 
-void dac_append(dac *dest, dac item) {
-  size_t new_count = dac_len(*dest) + dac_len(item) + 1;
+void dac_append(dac *dest, dac *item) {
+  size_t new_count = dac_len(dest) + dac_len(item) + 1;
   dac_reserve_capacity(dest, new_count);
   // copy src string at the NULL terminator of dest string
-  memcpy(dest->ptr + dac_len(*dest), item.ptr, dac_len(item) + 1);
+  memcpy(dest->ptr + dac_len(dest), item->ptr, dac_len(item) + 1);
   dest->count = new_count;
 }
 
-bool dac_contains(dac *s, dac search) {
-  char *s_str = dac_to_cstr(*s);
-  size_t s_len = dac_len(*s);
+bool dac_contains(dac *s, dac *search) {
+  char *s_str = dac_to_cstr(s);
+  size_t s_len = dac_len(s);
   char *search_str = dac_to_cstr(search);
   size_t search_len = dac_len(search);
 
   for (size_t i = 0; i < s_len; i++) {
     size_t j = 0;
-    if (s->ptr[i] == search.ptr[j]) {
+    if (s->ptr[i] == search->ptr[j]) {
       while (i + j < s_len && j < search_len && s_str[i + j] == search_str[j]) {
         j++;
       }
@@ -114,15 +113,15 @@ bool dac_contains(dac *s, dac search) {
   return false;
 }
 
-int dac_find(dac *s, dac search) {
-  char *s_str = dac_to_cstr(*s);
-  size_t s_len = dac_len(*s);
+int dac_find(dac *s, dac *search) {
+  char *s_str = dac_to_cstr(s);
+  size_t s_len = dac_len(s);
   char *search_str = dac_to_cstr(search);
   size_t search_len = dac_len(search);
 
   for (size_t i = 0; i < s_len; i++) {
     size_t j = 0;
-    if (s->ptr[i] == search.ptr[j]) {
+    if (s->ptr[i] == search->ptr[j]) {
       while (i + j < s_len && j < search_len && s_str[i + j] == search_str[j]) {
         j++;
       }
@@ -134,28 +133,28 @@ int dac_find(dac *s, dac search) {
   return -1;
 }
 
-void dac_replace(dac *s, dac search, dac item) {
+void dac_replace(dac *s, dac *search, dac *item) {
   int start = dac_find(s, search);
   if (start >= 0) {
     int delta = dac_len(item) - dac_len(search);
     if (delta > 0) {
       dac_reserve_capacity(s,
-                           dac_len(*s) + (dac_len(item) - dac_len(search)) + 1);
+                           dac_len(s) + (dac_len(item) - dac_len(search)) + 1);
       memmove(s->ptr + start + dac_len(item), s->ptr + start + dac_len(search),
-              dac_len(*s) + 1 - delta);
-      memcpy(s->ptr + start, item.ptr, dac_len(item));
+              dac_len(s) + 1 - delta);
+      memcpy(s->ptr + start, item->ptr, dac_len(item));
     } else if (delta < 0) {
-      memcpy(s->ptr + start, item.ptr, dac_len(item));
+      memcpy(s->ptr + start, item->ptr, dac_len(item));
       memmove(s->ptr + start + dac_len(item), s->ptr + start + dac_len(search),
-              dac_len(*s) + 1 + delta);
+              dac_len(s) + 1 + delta);
     } else {
-      memcpy(s->ptr + start, item.ptr, dac_len(item));
+      memcpy(s->ptr + start, item->ptr, dac_len(item));
     }
     s->count = s->count + delta;
   }
 }
 
-void dac_replace_all(dac *s, dac search, dac item) {
+void dac_replace_all(dac *s, dac *search, dac *item) {
   int start = dac_find(s, search);
   while (start >= 0) {
     dac_replace(s, search, item);
@@ -163,23 +162,23 @@ void dac_replace_all(dac *s, dac search, dac item) {
   }
 }
 
-void dac_append_many(dac *dest, dac *items, size_t items_count) {
-  size_t new_count = dac_len(*dest);
+void dac_append_many(dac *dest, dac **items, size_t items_count) {
+  size_t new_count = dac_len(dest);
   for (size_t i = 0; i < items_count; i++) {
-    new_count = new_count + items[i].count - 1;
+    new_count = new_count + items[i]->count - 1;
   }
   new_count = new_count + 1; // NULL terminator
 
   dac_reserve_capacity(dest, new_count);
   for (size_t i = 0; i < items_count; i++) {
-    memcpy(dest->ptr + dest->count - 1, items[i].ptr, items[i].count);
-    dest->count = dac_len(*dest) + dac_len(items[i]) + 1;
+    memcpy(dest->ptr + dest->count - 1, items[i]->ptr, items[i]->count);
+    dest->count = dac_len(dest) + dac_len(items[i]) + 1;
   }
   dest->count = new_count;
 }
 
-dac dac_join(dac *items, size_t items_count, dac delim) {
-  dac arr[items_count * 2 - 1];
+dac *dac_join(dac **items, size_t items_count, dac *delim) {
+  dac *arr[items_count * 2 - 1];
   size_t arr_len = DAC_ARRAY_LEN(arr);
   for (size_t i = 0; i < arr_len; i++) {
     if (i % 2 == 0) {
@@ -188,12 +187,12 @@ dac dac_join(dac *items, size_t items_count, dac delim) {
       arr[i] = delim;
     }
   }
-  dac new_str = dac_new("");
-  dac_append_many(&new_str, arr, arr_len);
+  dac *new_str = dac_new("");
+  dac_append_many(new_str, arr, arr_len);
   return new_str;
 }
 
-bool dac_starts_with(dac s, dac prefix) {
+bool dac_starts_with(dac *s, dac *prefix) {
   size_t s_len = dac_len(s);
   size_t prefix_len = dac_len(prefix);
   if (prefix_len == 0) {
@@ -202,7 +201,7 @@ bool dac_starts_with(dac s, dac prefix) {
 
   bool starts_with = true;
   for (size_t i = 0; i < prefix_len; i++) {
-    if (i < s_len && s.ptr[i] == prefix.ptr[i]) {
+    if (i < s_len && s->ptr[i] == prefix->ptr[i]) {
       continue;
     }
     return false;
@@ -210,7 +209,7 @@ bool dac_starts_with(dac s, dac prefix) {
   return starts_with;
 }
 
-bool dac_ends_with(dac s, dac suffix) {
+bool dac_ends_with(dac *s, dac *suffix) {
   size_t s_len = dac_len(s);
   size_t suffix_len = dac_len(suffix);
   if (suffix_len == 0) {
@@ -221,7 +220,7 @@ bool dac_ends_with(dac s, dac suffix) {
   for (size_t i = 0; i < suffix_len; i++) {
     if (s_len - 1 - (suffix_len - 1) + i > 0 &&
         s_len - 1 - (suffix_len - 1) + i < s_len &&
-        s.ptr[s_len - 1 - (suffix_len - 1) + i] == suffix.ptr[i]) {
+        s->ptr[s_len - 1 - (suffix_len - 1) + i] == suffix->ptr[i]) {
       continue;
     }
     return false;
@@ -231,9 +230,9 @@ bool dac_ends_with(dac s, dac suffix) {
 
 void dac_free(dac *s) { free(s->ptr); }
 
-void dac_free_many(dac items[], size_t items_count) {
+void dac_free_many(dac *items[], size_t items_count) {
   for (size_t i = 0; i < items_count; i++) {
-    free(items[i].ptr);
+    free(items[i]->ptr);
   }
 }
 
--- dac_test.c
+++ dac_test.c
@@ -51,9 +51,9 @@
 
 #define EXPECT_BOOL_EQ(e1, e2)                                                 \
   do {                                                                         \
-    char *s1 = e1 ? "true" : "false";                                          \
-    char *s2 = e2 ? "true" : "false";                                          \
-    EXPECT_EQ(s1, s2, e1 == e2, "'%s'");                                       \
+    char *_s1 = e1 ? "true" : "false";                                         \
+    char *_s2 = e2 ? "true" : "false";                                         \
+    EXPECT_EQ(_s1, _s2, e1 == e2, "'%s'");                                     \
   } while (0)
 
 #define EXPECT_NUMBER_CMP(e1, e2, condition, symbol, format)                   \
@@ -103,15 +103,17 @@ char *random_string(size_t length) {
 }
 
 void test_dac_new() {
-  dac s1 = dac_new("world");
+  dac *s1 = dac_new("world");
   EXPECT_STRING_EQ(dac_to_cstr(s1), "world");
   EXPECT_UINT_EQ(dac_len(s1), strlen("world"));
+  dac_free(s1);
 
   char *random_str = random_string(100);
-  dac s2 = dac_new(random_str);
+  dac *s2 = dac_new(random_str);
   EXPECT_STRING_EQ(dac_to_cstr(s2), random_str);
   EXPECT_UINT_EQ(dac_len(s2), strlen(random_str));
   free(random_str);
+  dac_free(s2);
 
   SUCCESS;
 }
@@ -119,113 +121,179 @@ void test_dac_new() {
 void test_dac_init() {
   dac s = {0};
   dac_init(&s, "HeLlo");
-  EXPECT_STRING_EQ(dac_to_cstr(s), "HeLlo");
-  EXPECT_UINT_EQ(dac_len(s), strlen("HeLlo"));
+  EXPECT_STRING_EQ(dac_to_cstr(&s), "HeLlo");
+  EXPECT_UINT_EQ(dac_len(&s), strlen("HeLlo"));
+  dac_free(&s);
 
   SUCCESS;
 }
 
 void test_dac_eq() {
-  EXPECT_BOOL_EQ(dac_eq(dac_new("hello"), dac_new("hello")), true);
-  EXPECT_BOOL_EQ(dac_eq(dac_new("hello"), dac_new("hell")), false);
+  dac *s = dac_new("hello");
+  dac *s1 = dac_new("hello");
+  dac *s2 = dac_new("hell");
+  EXPECT_BOOL_EQ(dac_eq(s, s1), true);
+  EXPECT_BOOL_EQ(dac_eq(s, s2), false);
+  dac_free(s);
+  dac_free(s1);
+  dac_free(s2);
   SUCCESS;
 }
 
 void test_dac_contains() {
-  dac s = dac_new("Hello world worldd 1234");
-  EXPECT_BOOL_EQ(dac_contains(&s, dac_new("ldd 1234")), true);
-  EXPECT_BOOL_EQ(dac_contains(&s, dac_new("dd 12345")), false);
-  EXPECT_BOOL_EQ(dac_contains(&s, dac_new("Hello world 12345")), false);
-  EXPECT_BOOL_EQ(dac_contains(&s, dac_new("worldd")), true);
+  dac *s = dac_new("Hello world worldd 1234");
+  dac *s1 = dac_new("ldd 1234");
+  dac *s2 = dac_new("dd 12345kj");
+  dac *s3 = dac_new("Hello world 12345");
+  dac *s4 = dac_new("worldd");
+  EXPECT_BOOL_EQ(dac_contains(s, s1), true);
+  EXPECT_BOOL_EQ(dac_contains(s, s2), false);
+  EXPECT_BOOL_EQ(dac_contains(s, s3), false);
+  EXPECT_BOOL_EQ(dac_contains(s, s4), true);
+  dac_free(s);
+  dac_free(s1);
+  dac_free(s2);
+  dac_free(s3);
+  dac_free(s4);
   SUCCESS;
 }
 
 void test_dac_find() {
-  dac s = dac_new("Hello world worldd 1234");
-  EXPECT_INT_EQ(dac_find(&s, dac_new("ldd 1234")), 15);
-  EXPECT_INT_EQ(dac_find(&s, dac_new("dd 12345")), -1);
-  EXPECT_INT_EQ(dac_find(&s, dac_new("Hello world 12345")), -1);
-  EXPECT_INT_EQ(dac_find(&s, dac_new("worldd")), 12);
+  dac *s = dac_new("Hello world worldd 1234");
+  dac *s1 = dac_new("ldd 1234");
+  dac *s2 = dac_new("dd 12345kj");
+  dac *s3 = dac_new("Hello world 12345");
+  dac *s4 = dac_new("worldd");
+  EXPECT_INT_EQ(dac_find(s, s1), 15);
+  EXPECT_INT_EQ(dac_find(s, s2), -1);
+  EXPECT_INT_EQ(dac_find(s, s3), -1);
+  EXPECT_INT_EQ(dac_find(s, s4), 12);
+  dac_free(s);
+  dac_free(s1);
+  dac_free(s2);
+  dac_free(s3);
+  dac_free(s4);
   SUCCESS;
 }
 
 void test_dac_replace() {
-  dac s1 = dac_new("Hello hello hello world");
-  dac_replace(&s1, dac_new("hello"), dac_new("jim"));
+  dac *search = dac_new("hello");
+  dac *replace1 = dac_new("jim");
+  dac *replace2 = dac_new("jimjim");
+
+  dac *s1 = dac_new("Hello hello hello world");
+  dac_replace(s1, search, replace1);
   EXPECT_STRING_EQ(dac_to_cstr(s1), "Hello jim hello world");
   EXPECT_UINT_EQ(dac_len(s1), strlen("Hello jim hello world"));
 
-  dac s2 = dac_new("Hello hello hello world");
-  dac_replace(&s2, dac_new("hello"), dac_new("jimjim"));
+  dac *s2 = dac_new("Hello hello hello world");
+  dac_replace(s2, search, replace2);
   EXPECT_STRING_EQ(dac_to_cstr(s2), "Hello jimjim hello world");
   EXPECT_UINT_EQ(dac_len(s2), strlen("Hello jimjim hello world"));
+  dac_free(search);
+  dac_free(replace1);
+  dac_free(replace2);
+  dac_free(s1);
+  dac_free(s2);
   SUCCESS;
 }
 
 void test_dac_replace_all() {
-  dac s1 = dac_new("Hello hello hello world");
-  dac_replace_all(&s1, dac_new("hello"), dac_new("jim"));
+  dac *search = dac_new("hello");
+  dac *replace1 = dac_new("jim");
+  dac *replace2 = dac_new("jimjim");
+
+  dac *s1 = dac_new("Hello hello hello world");
+  dac_replace_all(s1, search, replace1);
   EXPECT_STRING_EQ(dac_to_cstr(s1), "Hello jim jim world");
   EXPECT_UINT_EQ(dac_len(s1), strlen("Hello jim jim world"));
 
-  dac s2 = dac_new("Hello hello hello world");
-  dac_replace_all(&s2, dac_new("hello"), dac_new("jimjim"));
+  dac *s2 = dac_new("Hello hello hello world");
+  dac_replace_all(s2, search, replace2);
   EXPECT_STRING_EQ(dac_to_cstr(s2), "Hello jimjim jimjim world");
   EXPECT_UINT_EQ(dac_len(s2), strlen("Hello jimjim jimjim world"));
+  dac_free(search);
+  dac_free(replace1);
+  dac_free(replace2);
+  dac_free(s1);
+  dac_free(s2);
   SUCCESS;
 }
 
 void test_dac_append() {
-  dac s = dac_new("hello");
-  dac_append(&s, dac_new("world"));
+  dac *s = dac_new("hello");
+  dac *s2 = dac_new("world");
+  dac_append(s, s2);
   EXPECT_STRING_EQ(dac_to_cstr(s), "helloworld");
   EXPECT_UINT_EQ(dac_len(s), strlen("helloworld"));
   EXPECT_STRING_EQ(dac_to_cstr(s), "helloworld");
   EXPECT_UINT_EQ(dac_len(s), strlen("helloworld"));
+  dac_free(s);
+  dac_free(s2);
   SUCCESS;
 }
 
 void test_dac_append_many() {
-  dac s = dac_new("01");
-  dac arr[3] = {
+  dac *s = dac_new("01");
+  dac *arr[3] = {
       dac_new("234"),
       dac_new("5678"),
       dac_new("9abcdef"),
   };
-  dac_append_many(&s, arr, 3);
+  dac_append_many(s, arr, 3);
   EXPECT_STRING_EQ(dac_to_cstr(s), "0123456789abcdef");
   EXPECT_UINT_EQ(dac_len(s), strlen("0123456789abcdef"));
+  dac_free(s);
+  dac_free_many(arr, 3);
   SUCCESS;
 }
 
 void test_dac_join() {
-  dac arr[4] = {
+  dac *arr[4] = {
       dac_new("1"),
       dac_new("2"),
       dac_new("3"),
       dac_new("4"),
   };
-  dac s = dac_join(arr, DAC_ARRAY_LEN(arr), dac_new(",.,"));
+  dac *delim = dac_new(",.,");
+  dac *s = dac_join(arr, DAC_ARRAY_LEN(arr), delim);
   EXPECT_STRING_EQ(dac_to_cstr(s), "1,.,2,.,3,.,4");
   EXPECT_UINT_EQ(dac_len(s), strlen("1,.,2,.,3,.,4"));
+  dac_free(s);
+  dac_free(delim);
+  dac_free_many(arr, 3);
   SUCCESS;
 }
 
 void test_dac_starts_with() {
-  dac s = dac_new("Hello world");
+  dac *s = dac_new("Hello world");
+  dac *s1 = dac_new("Hello w");
+  dac *s2 = dac_new("Hello world2");
+  dac *s3 = dac_new("");
 
-  EXPECT_BOOL_EQ(dac_starts_with(s, dac_new("Hello w")), true);
-  EXPECT_BOOL_EQ(dac_starts_with(s, dac_new("Hello world2")), false);
-  EXPECT_BOOL_EQ(dac_starts_with(s, dac_new("")), false);
+  EXPECT_BOOL_EQ(dac_starts_with(s, s1), true);
+  EXPECT_BOOL_EQ(dac_starts_with(s, s2), false);
+  EXPECT_BOOL_EQ(dac_starts_with(s, s3), false);
+  dac_free(s);
+  dac_free(s1);
+  dac_free(s2);
+  dac_free(s3);
   SUCCESS;
 }
 
 void test_dac_ends_with() {
-  dac s = dac_new("Hello world");
+  dac *s = dac_new("Hello world");
+  dac *s1 = dac_new("o world");
+  dac *s2 = dac_new("1Hello world");
+  dac *s3 = dac_new("");
 
-  EXPECT_BOOL_EQ(dac_ends_with(s, dac_new("o world")), true);
-  EXPECT_BOOL_EQ(dac_ends_with(s, dac_new("1Hello world")), false);
-  EXPECT_BOOL_EQ(dac_ends_with(s, dac_new("")), false);
+  EXPECT_BOOL_EQ(dac_ends_with(s, s1), true);
+  EXPECT_BOOL_EQ(dac_ends_with(s, s2), false);
+  EXPECT_BOOL_EQ(dac_ends_with(s, s3), false);
+  dac_free(s);
+  dac_free(s1);
+  dac_free(s2);
+  dac_free(s3);
   SUCCESS;
 }
 
